b0ac2d58f1288dd4887d7141b97da5ca02346651
Piotr Gaszcz <piotr.gaszcz@gmail.com>
2015-09-25 14:41:57 +0200

    MissingInteger completed tests

--- src/test/java/lesson2/MissingIntegerTest.java
+++ src/test/java/lesson2/MissingIntegerTest.java
@@ -15,9 +15,38 @@ public class MissingIntegerTest {
 	}
 
 	@Test
-	public void test() {
-		int A[] = {};
-		assertEquals(0, missingInteger.solution(A));
+	public void testOneElementArray() {
+		int A[] = {-100};
+		assertEquals(1, missingInteger.solution(A));
+		
+		int B[] = {100};
+		assertEquals(99, missingInteger.solution(B));
 	}
 
+	@Test
+	public void testAllNegativeElementsArray() {
+		int A[] = {-100, -8, -20, -11};
+		assertEquals(1, missingInteger.solution(A));
+	}
+	
+	@Test
+	public void testHighElementsArray() {
+		int A[] = {100, 81, 201, 110};
+		assertEquals(80, missingInteger.solution(A));
+	}
+	
+	@Test
+	public void testWtithZeroElementsArray() {
+		int A[] = {0, 2, 100, 81, 201, 110};
+		assertEquals(1, missingInteger.solution(A));
+	}
+	
+	@Test
+	public void testPositiveElementsArray() {
+		int A[] = {0, 1, 2, 3, 100, 81, 201, 110};
+		assertEquals(4, missingInteger.solution(A));
+		
+		int B[] = {1, 3, 6, 4, 1, 2};
+		assertEquals(5, missingInteger.solution(B));
+	}
 }
